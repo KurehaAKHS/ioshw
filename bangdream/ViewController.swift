d3dbf0840de26bd4de999396586b75d73b390fa4
KurehaAKHS <zz462428zz@gmail.com>
2018-10-16 20:18:33 +0800

    Add files via upload

---
+++ bangdream/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  bangdream
+//
+//  Created by user_10 on 2017/4/6.
+//  Copyright © 2017年 user_10. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
